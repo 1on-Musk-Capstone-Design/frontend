cf6136353f84c685042a49dfbb59c199becaa9b9
kimyeongseo-gg <kygs0788@naver.com>
2025-10-10 00:24:47 -0700

    feat: 메인 페이지(MainPage) 및 카드 기반 프로젝트 네비게이션 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import InfiniteCanvasPage from './pages/infinite-canvas/InfiniteCanvasPage';
+import MainPage from './pages/MainPage/MainPage';
 import './index.css';
 import Login from './pages/login/Login';
 
@@ -8,8 +9,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 기본 경로를 무한 캔버스로 설정 */}
-        <Route path="/" element={<InfiniteCanvasPage />} />
+  {/* 기본 경로를 메인 페이지로 변경 */}
+  <Route path="/" element={<MainPage />} />
         <Route path="/canvas" element={<InfiniteCanvasPage />} />
         <Route path="/canvas/:projectId" element={<InfiniteCanvasPage />} />
         {/* 나중에 추가될 다른 페이지들 */}
